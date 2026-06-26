73ab03e386db596bc218a3be8ddb2c10cfdaea5c
Zhouyuan <lizhouyuan0527@outlook.com>
2026-06-26 14:39:38 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+const CurrentYear = new Date().getFullYear();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("cover.ejs", { footerYear: CurrentYear });
+});
+
+app.get("/cover", (req, res) => {
+  res.render("cover.ejs", { footerYear: CurrentYear });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs", { footerYear: CurrentYear });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs", { footerYear: CurrentYear });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
