9aa19cdca0fd7a996e6eaa325917af664a070e14
Zhouyuan <lizhouyuan0527@outlook.com>
2026-06-30 10:25:31 -0400

    connect to supabase

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import express from "express";
+import env from "dotenv";
+import { createClient } from "@supabase/supabase-js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+env.config();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY,
+);
+
 const CurrentYear = new Date().getFullYear();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("cover.ejs", { footerYear: CurrentYear });
+
+  try {
+    let { data: User, error } = await supabase.from("User").select("*");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get("/cover", (req, res) => {
